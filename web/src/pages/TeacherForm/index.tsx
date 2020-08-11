77c004ab1ef3817e1105b2b8c3d8a432504e889e
Marlon Mavinier <marlon.moura@hbsis.com.br>
2020-08-10 21:09:57 -0300

    Front pagina de cadastro 0:37

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+import './styles.css';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-    <PageHeader title="Que incrével que você quer dar aulas."/>
-  </div>
+      <PageHeader
+        title="Que incrével que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário da inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subjec" label="Matéria" />
+          <Input name="cost" label="custo da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados.
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
+      </main>
+    </div>
   );
 };
 
